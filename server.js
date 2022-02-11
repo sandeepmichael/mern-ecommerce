303645cb584cda07545ef343253ff3433ce80a9a
Mahadari Sandeep <mahadarisandeep@gmail.com>
2022-02-11 13:35:43 +0530

    another commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/orders', require('./routes/orderRoutes'))
 
 
 if(process.env.NODE_ENV === 'Production'){
-    app.use('/', express.static('client/build'))
+    app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
