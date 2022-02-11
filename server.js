cf0946313106e8a2a9613130e231a7f64fa1aac6
Mahadari Sandeep <mahadarisandeep@gmail.com>
2022-02-11 13:24:23 +0530

    another commit

--- server.js
+++ server.js
@@ -20,19 +20,16 @@ app.use('/api/orders', require('./routes/orderRoutes'))
 
 
 if(process.env.NODE_ENV === 'Production'){
-    app.use(express.static('client/build'))
+    app.use('/', express.static('client/build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
     })
 }
 
-app.get('/', (req, res) => {
-    res.send('api is running....')
-})
 
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log(`server is running on ${process.env.NODE_ENV} mode port 5000`);
+    console.log(`server is running on port 5000`);
 })
