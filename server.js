8c67c6dfc5679997c4a9fd08d62675a657abe989
Mahadari Sandeep <mahadarisandeep@gmail.com>
2022-02-11 13:42:21 +0530

    added node env

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/orders', require('./routes/orderRoutes'))
 
 
-if(process.env.NODE_ENV === 'Production'){
-    app.use(express.static('client/build'))
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static('client/build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
